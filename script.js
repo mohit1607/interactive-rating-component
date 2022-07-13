3e96718a37d229181fec6585eb2d26f439eb89de
Mohit Ashiliya <mohit.ashliya@gmail.com>
2022-07-13 17:44:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const allRating = document.querySelectorAll('.rating')
+let selected = undefined;
+const resetAll = () => {
+    allRating.forEach((curr) => {
+        curr.classList = "rating"
+    })
+}
+allRating.forEach((current,index) => {
+    current.addEventListener('click', () => {
+        resetAll()
+        selected = index
+        current.classList = "selected"
+    })
+})
+
+const submitButton = document.getElementById('submit')
+
+submitButton.addEventListener('click', () => {
+    let selectedItem = document.querySelector('.chip')
+    if(selected !== undefined){
+        selectedItem.textContent = `You selected ${selected} out of 5`
+        document.querySelector('.container2').style.display = 'flex'
+        document.querySelector('.container').style.display = 'none'
+    }
+    else{
+        alert('select the rating first.')
+    }
+})
